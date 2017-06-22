f012d6a45964453b4db8ecab45eaafffddce4155
diederikcraps <diederik127@hotmail.com>
2017-06-22 13:20:44 +0200

    Oplevering juni 2017

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# creativity-gym
+# Creativity Gym Bolt Theme
+Deze theme werd ontworpen door [Robert Vaida](http://www.code-master.be) en ontwikkeld door [Designosource](http://www.designosource.be). Voor vragen na het lezen van deze readme kan je best contact opnemen met [Diederik Craps](http://www.diederikcraps.be).
+
+## Aanpassingen doen?
+Deze theme werd ontwikkeld in het CMS [Bolt 3.2](http://www.bolt.cm) en werkt met **.twig templates**. Om deze theme aan te passen ga je als volgt te werk:
+
+1. Voer een [update](https://docs.bolt.cm/3.2/upgrading/updating#updating-on-the-command-line) uit van het Bolt CMS.
+
+2. Download de database file van de live versie. Deze staat in de folder ```/app/database```.
+Login voor de productiesite vraag je aan [Joris Hens](https://twitter.com/GoodBytes).
+
+3. Maak een kopie van de ```/public/theme/gym``` folder. Pas de lijn ```theme: gym``` in de file ```/app/config/config.yml``` aan met de naam van het nieuwe theme.
+
+4. Klaar om te themen! Terminal openen, met het ```cd```-commando naar de folder ```/public/theme/NAAM_VAN_HET_THEME/assets``` navigeren en [Sass](http://sass-lang.com/) watch opzetten met ```sass --watch sass/screen.scss:css/screen.css```.
+
+5. **Happy theming!**
---
+++ public/theme/gym/assets/js/map.js
@@ -0,0 +1,231 @@
+/******************************************
+ ******  GOOGLE MAP CUSTOMIZATION   ********
+ ******************************************/
+
+// Google map
+google.maps.event.addDomListener(window, 'load', init);
+var map;
+
+function init() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.024701, 4.487487),
+        zoom: 16,
+        zoomControl: false,
+        disableDoubleClickZoom: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        scrollwheel: false,
+        panControl: false,
+        streetViewControl: false,
+        draggable: true,
+        overviewMapControl: false,
+        overviewMapControlOptions: {
+            opened: false,
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [{
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#f5f5f5"
+                }]
+            },
+            {
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#616161"
+                }]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "color": "#f5f5f5"
+                }]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#bdbdbd"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#eeeeee"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#757575"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#e5e5e5"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#9e9e9e"
+                }]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#ffffff"
+                }]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#757575"
+                }]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#dadada"
+                }]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#616161"
+                }]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#9e9e9e"
+                }]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#e5e5e5"
+                }]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#eeeeee"
+                }]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#c9c9c9"
+                }]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#9e9e9e"
+                }]
+            }
+        ],
+    }
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var locations = [
+        ['The Creativity Gym', "Lange Ridderstraat 44<br>2800 Mechelen", 'undefined', 'undefined', 'undefined', 51.0247793, 4.4847822, 'https://mapbuildr.com/assets/img/markers/ellipse-green.png']
+    ];
+    for (i = 0; i < locations.length; i++) {
+        if (locations[i][1] == 'undefined') {
+            description = '';
+        } else {
+            description = locations[i][1];
+        }
+        if (locations[i][2] == 'undefined') {
+            telephone = '';
+        } else {
+            telephone = locations[i][2];
+        }
+        if (locations[i][3] == 'undefined') {
+            email = '';
+        } else {
+            email = locations[i][3];
+        }
+        if (locations[i][4] == 'undefined') {
+            web = '';
+        } else {
+            web = locations[i][4];
+        }
+        if (locations[i][7] == 'undefined') {
+            markericon = '';
+        } else {
+            markericon = locations[i][7];
+        }
+        marker = new google.maps.Marker({
+            icon: markericon,
+            position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+            map: map,
+
+            title: locations[i][0],
+            desc: description,
+            tel: telephone,
+            email: email,
+            web: web
+        });
+        if (web.substring(0, 7) != "http://") {
+            link = "http://" + web;
+        } else {
+            link = web;
+        }
+        bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
+    }
+
+    function bindInfoWindow(marker, map, title, desc, telephone, email, web, link) {
+        var infoWindowVisible = (function() {
+            var currentlyVisible = false;
+            return function(visible) {
+                if (visible !== undefined) {
+                    currentlyVisible = visible;
+                }
+                return currentlyVisible;
+            };
+        }());
+        iw = new google.maps.InfoWindow();
+        var html = "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h4>" + title + "</h4><p>" + desc + "</p></div>";
+        iw = new google.maps.InfoWindow({
+            content: html
+        });
+        iw.open(map, marker);
+        infoWindowVisible(true);
+        marker.setPosition(map.getCenter());
+        google.maps.event.addListener(marker, 'click', function() {
+            if (infoWindowVisible()) {
+                iw.close();
+                infoWindowVisible(false);
+            } else {
+                iw.open(map, marker);
+                infoWindowVisible(true);
+            }
+        });
+        google.maps.event.addListener(iw, 'closeclick', function() {
+            infoWindowVisible(false);
+        });
+    }
+}
